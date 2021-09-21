41f89c4eef8f91eaf1e31f9aaa8331ffc92c7dce
prathod1995 <prathod@deqode.com>
2021-09-21 16:48:00 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "access_key"
+  secret_key = "secret_key"
+}
+
+resource "aws_instance" "us-east-2" {
+    ami = "ami-00dfe2c7ce89a450b"
+    instance_type = "t2.micro"
+
+     tags = {
+    Name = "tf-example"
+  }
+}
